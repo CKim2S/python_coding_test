1297d560a8d7af59acb4b8fefc2df62f2f2c08a2
C. Kim <96875410+CKim2S@users.noreply.github.com>
2023-08-29 21:44:40 +0900

    Create problem_12915.py

---
+++ problem_12915.py
@@ -0,0 +1,22 @@
+def solution(strings, n):
+    answer = []
+    real_answer = []
+
+    for string in strings:
+        nth_word = string[n]
+        answer.append(nth_word)
+
+    answer.sort()
+
+    for word in answer:
+        for string in strings:
+            if string[n] == word:
+                real_answer.append(string)
+                
+    real_answer = set(real_answer)
+    real_answer = list(real_answer)
+    real_answer.sort()
+    return real_answer
+
+
+print(solution(["abce", "abcd", "cdx"], 2))
